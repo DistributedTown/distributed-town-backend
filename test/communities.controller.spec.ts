ed5161776cef825f393e32fe46b9724b10e745e3
migrenaa <mimonova13@gmail.com>
2021-02-25 10:40:14 +0200

    added dummy test for configuring the ci/cd flow

---
+++ test/communities.controller.spec.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import { expect } from "chai";
+import "mocha";
+
+
+describe("dummy test", () => {
+  it("should pass", async () => {
+    expect(true, true);
+  })
+});
